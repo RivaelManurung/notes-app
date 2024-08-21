b5bbbaa9d3a714b9f3b5643650b80c34311b6920
RivaelManurung <135939684+RivaelManurung@users.noreply.github.com>
2024-08-21 18:15:06 +0700

    notes

--- frontend/notes-app/src/App.jsx
+++ frontend/notes-app/src/App.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
-import Home from './pages/Home/Home'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import SignUp from "./pages/SignUp/SignUp";
+
+
+const routes = (
+  <Router>
+    <Routes>
+      <Route path="/dashboard" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+    </Routes>
+  </Router>
+)
 
 const App = () => {
   return (
-    <div>
-      <Home/>
+    <div className="bg-blue-500 text-white p-4">
+      Tailwind CSS is working!
     </div>
-  )
-}
+  );
+};
 
-export default App
+
+
+export default App;
